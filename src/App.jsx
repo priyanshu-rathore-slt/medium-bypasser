a2f598f8e846a89cb9fc37c64a4ebcb30b308a99
priyanshu-rathore-slt <priyanshu@sltechnocrats.net>
2024-10-09 18:43:43 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <div style={{display:"flex",flexDirection:"column",gap:"5rem",justifyContent:"center",alignItems:"center"}}>
       <h1 style={{fontSize:"20px",fontWeight:"bold",}}>Medium Bypasser</h1>
-      <textarea type="text" value={state} onChange={(e)=>setState(e.target.value)} />
+      <textarea type="text" placeholder='Paste medium link' value={state} onChange={(e)=>setState(e.target.value)} />
       <button onClick={onClickhandler}>Submit</button>
     </div>
   )
